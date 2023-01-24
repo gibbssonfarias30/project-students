f6fc3a538a2b8ff95ad959ff2c15f9bf9f7ad141
Gibbsson <gibbfarc17@gmail.com>
2023-01-24 16:46:37 -0500

    Add angular material, changes in students.component and app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/components/students/students.component';
 import { NewStudentComponent } from './students/components/new-student/new-student.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { NewStudentComponent } from './students/components/new-student/new-stude
     imports: [
         BrowserModule,
         AppRoutingModule,
-        HttpClientModule
+        HttpClientModule,
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/students/components/students/students.component.ts
+++ src/app/students/components/students/students.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Student, StudentPage } from '../../shared/student.model';
+import { StudentService } from '../../shared/student.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
-  selector: 'app-students',
-  templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+    selector: 'app-students',
+    templateUrl: './students.component.html',
+    styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+    studentPage?: StudentPage;
+    displayedColumns = ['titulo', 'precio', 'fechaCreacion', 'acciones']
 
-  constructor() { }
+    constructor(private studentService: StudentService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.getAll();
+    }
+    getAll() {
+        this.studentService.paginar()
+            .subscribe(studentPage => {
+                console.log("Students:", studentPage);
+                this.studentPage = studentPage;
+            })
+    }
+
+    deleteStudent(student: Student) {
+        if (!confirm('¿Estás seguro de eliminar este libro?')) {
+            return;
+        }
+
+        this.studentService.delete(student.id)
+            .subscribe(() => {
+                this.getAll();
+            })
+    }
+
+
+    paginarLibros(event: PageEvent) {
+        const size = event.pageSize;
+        const page = event.pageIndex;
+
+        this.studentService.paginar(size, page)
+            .subscribe(studentPage => {
+                console.log("Libros:", studentPage);
+                this.studentPage = studentPage;
+            })
+    }
+
 
 }
